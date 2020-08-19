4b900f8b2dd50e282d65e2406049ef5b876750b8
MatiasVDB <matiasvdb@hotmail.com>
2020-08-18 22:33:10 -0300

    123

--- js/codigo.js
+++ js/codigo.js
@@ -1,96 +1,166 @@
-// $(document).ready(function(e) {
-//     $("#listaNachosZapatistas").toggle(
-
-//         function() {
-//             $("#listaNachosZapatistas").hide(e);
-//             $("#texto").hide(e);
-
-//         },
-
-//         $("#nachos").click(function(e) {
-
-
-//             $("#listaNachosZapatistas").show(e);
-//             $("#nachisimos").show(e);
-
-//         }),
-
-//         $("#nachos").click(function(e) {
-
-
-//             $("#listaNachosZapatistas").hide(e);
-//             $("#nachisimos").hide(e);
-
-//         })
-
-
-//     );
-// });
-
-
-
 $(document).ready(function(e) {
 
     $("#listaNachosZapatistas").toggle(
 
     );
-    $("#nachisimos").toggle(
+
+    $("#listaNachisimos").toggle(
+
+    );
+    $("#listaChicharronesQuesoVegetariano").toggle(
+
+    );
+
+    $("#listaChicharronesQuesoArgentino").toggle(
+
+    );
+
+    $("#listaPapasZapatistas").toggle(
+
+    );
+
+    $("#listaPechugitasBuffaloRecargadas").toggle(
+
+    );
+
+    $("#listaTamalesEnCazuela").toggle(
+
+    );
+
+    $("#listaSopasAzteca").toggle(
+
+    );
+
+    $("#listaSopadeLima").toggle(
 
     );
 
 });
 
-var contador = 0;
 
-// while (contador != 2) {
+
 $(document).ready(function(e) {
+    var contador = 0;
+    $("#contenedorBotonNachos").click(function(e) {
+        console.log(contador)
 
-    $("#nachos").click(function(e) {
 
-        contador++;
+        if (contador == 0) {
+            $("#listaNachosZapatistas").show(e);
+            $("#listaNachisimos").show(e);
 
-        $("#listaNachosZapatistas").show(e);
-        $("#nachisimos").show(e);
-
-        if (contador == 2) {
-
+            contador++;
+        } else {
             $("#listaNachosZapatistas").hide(e);
-            $("#nachisimos").hide(e);
+            $("#listaNachisimos").hide(e);
 
             contador = 0;
-
         }
-
     });
-
-
 
 });
 
 
+$(document).ready(function(e) {
+    var contador = 0;
+    $("#contenedorBotonChicharrones").click(function(e) {
+        console.log(contador)
+
+        if (contador == 0) {
+
+            $("#listaChicharronesQuesoVegetariano").show(e);
+            $("#listaChicharronesQuesoArgentino").show(e);
+
+            contador++;
+        } else {
+
+            $("#listaChicharronesQuesoVegetariano").hide(e);
+            $("#listaChicharronesQuesoArgentino").hide(e);
+
+            contador = 0;
+        }
+    });
+
+});
+
+$(document).ready(function(e) {
+    var contador = 0;
+    $("#contenedorBotonPapas").click(function(e) {
+        console.log(contador)
 
 
+        if (contador == 0) {
+            $("#listaPapasZapatistas").show(e);
 
 
+            contador++;
+        } else {
+            $("#listaPapasZapatistas").hide(e);
+
+            contador = 0;
+        }
+    });
+
+});
+
+$(document).ready(function(e) {
+    var contador = 0;
+    $("#contenedorBotonPechugitas").click(function(e) {
+        console.log(contador)
 
 
+        if (contador == 0) {
+            $("#listaPechugitasBuffaloRecargadas").show(e);
 
 
+            contador++;
+        } else {
+            $("#listaPechugitasBuffaloRecargadas").hide(e);
 
-// }
+            contador = 0;
+        }
+    });
 
-// $(document).ready(function(e) {
+});
 
-//     if (contador > 2) {
+$(document).ready(function(e) {
+    var contador = 0;
+    $("#contenedorBotonTamales").click(function(e) {
+        console.log(contador)
 
-//         $("#nachos").click(function(e) {
+
+        if (contador == 0) {
+            $("#listaTamalesEnCazuela").show(e);
 
 
-//             $("#listaNachosZapatistas").hide(e);
-//             $("#nachisimos").hide(e);
+            contador++;
+        } else {
+            $("#listaTamalesEnCazuela").hide(e);
 
-//         });
+            contador = 0;
+        }
+    });
 
-//     }
+});
 
-// });
+$(document).ready(function(e) {
+    var contador = 0;
+    $("#contenedorBotonSopas").click(function(e) {
+        console.log(contador)
+
+        if (contador == 0) {
+
+            $("#listaSopasAzteca").show(e);
+            $("#listaSopadeLima").show(e);
+
+            contador++;
+        } else {
+
+            $("#listaSopasAzteca").hide(e);
+            $("#listaSopadeLima").hide(e);
+
+            contador = 0;
+        }
+    });
+
+});
